69a6ac99881b61e6e9518f1bab05333cbbc6a7ce
Hawa <hawa.francis@gmail.com>
2021-06-08 20:23:08 -0400

    updated route

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const db = require("../models");
 
-// get workouts
+// get last workout
 router.get("/api/workouts", (req, res) => {
   db.Workout.find({})
     .sort({ date: -1 })
@@ -11,6 +11,20 @@ router.get("/api/workouts", (req, res) => {
     .catch((err) => {
       res.status(400).json(err);
     });
+});
+
+// workouts in range
+router.get("/api/workouts/range", (req, res) => {
+
+    db.Workout.find({}).then(dbWorkout => {
+
+        console.log(dbWorkout);
+
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+
 });
 
   
@@ -33,6 +47,8 @@ router.put("/api/workouts/:id", async (req, res) => {
         res.status(400).json(err);
       });
   });
+
+
 
 // new workout
 router.post("/api/workouts", ({ body }, res) => {
